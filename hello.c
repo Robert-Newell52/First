543d81f666f4f36209f438a024ad4a213ae2905f
Robert-Newell52 <rnewell10@huskers.unl.edu>
2020-08-17 22:37:22 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Author: Robert Newell
+ * Date: 2020/08/16
+ *
+ * Hello to the world
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Good Evening!\n");
+    printf("Good Evening!\n");
+    printf("Good Evening!\n");
+    printf("Good Evening!\n");
+    printf("Good Evening!\n");
+    printf("Good Evening!\n");
+    printf("Good Evening!\n");
+
+    return 0;
+ }
---
+++ info.md
@@ -0,0 +1,2 @@
+Name: Robert Newell
+email: rnewell10@huskers.unl.edu
